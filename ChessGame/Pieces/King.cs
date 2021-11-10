266ca452e39ca035454a79c6b31b83ab2418022b
Gustav Bengtsson <56593926+bengtsson6@users.noreply.github.com>
2021-11-10 15:35:13 +0100

    King movement implemented

--- ChessGame/Pieces/King.cs
+++ ChessGame/Pieces/King.cs
@@ -5,16 +5,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ChessGame.Utils;
 
 namespace ChessGame.Pieces
 {
     public class King : Piece
     {
+        private bool isFirstMove;
         public King(Cordinate cordinate, Alliance alliance, PieceType pieceType) : base(cordinate, alliance ,pieceType)
         {
-
+            IsFirstMove = true;
         }
+
+        public bool IsFirstMove { get => isFirstMove; set => isFirstMove = value; }
+
         public override List<Move> LegalMoves(Board board)
+        {
+            List<Move> legalMoves = new List<Move>();
+            Cordinate[][] straigthLineCordinates = MoveUtils.CalculateStraigthLineMovement(this.Cordinate);
+            Cordinate[][] diagonalMovementCordinates = MoveUtils.CalculateDiagonalMovement(this.Cordinate);
+            Cordinate[][] candidateCordinates = straigthLineCordinates.Concat(diagonalMovementCordinates).ToArray();
+
+            for (int i = 0; i < (MoveUtils.DiagonalMoveDirections + MoveUtils.LinearMoveDirections); i++)
+            {
+                if (candidateCordinates[i].Length != 0)
+                {
+                    Cordinate currentCandidateCordinate = candidateCordinates[i][0];
+                    Tile possibleDestinationTile = board.GetTile(currentCandidateCordinate);
+                    if (!possibleDestinationTile.IsTileOccupied())
+                    {
+                        legalMoves.Add(new NonCaptureMove(this, currentCandidateCordinate, board));
+                    } 
+                    else
+                    {
+                        Piece pieceOnOccupiedTile = possibleDestinationTile.GetPiece();
+                        if(pieceOnOccupiedTile.Alliance != this.Alliance)
+                        {
+                            legalMoves.Add(new CaptureMove(this, currentCandidateCordinate, board, pieceOnOccupiedTile));
+                        }
+                    }
+                }
+            }
+            if (IsCastlePossilbe())
+            {
+
+            }
+            
+            return legalMoves;
+        }
+
+        private bool IsCastlePossilbe()
         {
             throw new NotImplementedException();
         }
--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -16,21 +16,7 @@ namespace ChessGame
             Board board = Board.SetPiecesStartPosition();
             Console.WriteLine(board.ToString());
             Console.ReadLine();
-           
-            Cordinate[][] linearMovesCordinates = MoveUtils.CalculateStraigthLineMovement(new Cordinate(3,3));
-            Cordinate[][] diagonalMovesCordinates = MoveUtils.CalculateDiagonalMovement(new Cordinate(3, 3));
-
-            Cordinate[][] candidateCordinates = linearMovesCordinates.Concat(diagonalMovesCordinates).ToArray();
-
-            for (int i = 0; i < candidateCordinates.Length; i++)
-            {
-                foreach (Cordinate cor in candidateCordinates[i])
-                {
-                    Console.WriteLine(cor.XCordinate + ", " + cor.YCordinate);
-                }
-            }
-
-            Console.ReadLine();
+ 
         }
     }
 }
